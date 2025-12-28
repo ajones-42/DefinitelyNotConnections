e223cc3a0f499f09c7a6fd129ead3b6f9de3c817
ajones-42 <alexander.r.jones11@gmail.com>
2025-12-28 18:17:00 +0100

    Make GuessBuilder and GameStateBuilder classes.

--- DefinitelyNotConnectionsTests/Builders/GameStateBuilder.swift
+++ DefinitelyNotConnectionsTests/Builders/GameStateBuilder.swift
@@ -7,34 +7,34 @@
 
 import Foundation
 
-struct GameStateBuilder {
+class GameStateBuilder {
     var gamePhase: GamePhase = .playing
     var numMistakesRemaining: Int = 4
     var completedCategories: [Category] = []
     var guesses: [Guess] = []
     var remainingClueBoxes: [ClueBox] = []
     
-    mutating func withGamePhase(gamePhase: GamePhase) -> GameStateBuilder {
+    func withGamePhase(gamePhase: GamePhase) -> GameStateBuilder {
         self.gamePhase = gamePhase
         return self
     }
     
-    mutating func withNumMistakesRemaining(numMistakesRemaining: Int) -> GameStateBuilder {
+    func withNumMistakesRemaining(numMistakesRemaining: Int) -> GameStateBuilder {
         self.numMistakesRemaining = numMistakesRemaining
         return self
     }
     
-    mutating func withCompletedCategories(completedCategories: [Category]) -> GameStateBuilder {
+    func withCompletedCategories(completedCategories: [Category]) -> GameStateBuilder {
         self.completedCategories = completedCategories
         return self
     }
     
-    mutating func withGuesses(guesses: [Guess]) -> GameStateBuilder {
+    func withGuesses(guesses: [Guess]) -> GameStateBuilder {
         self.guesses = guesses
         return self
     }
     
-    mutating func withRemainingClueBoxes(remainingClueBoxes: [ClueBox]) -> GameStateBuilder {
+    func withRemainingClueBoxes(remainingClueBoxes: [ClueBox]) -> GameStateBuilder {
         self.remainingClueBoxes = remainingClueBoxes
         return self
     }
--- DefinitelyNotConnectionsTests/Builders/GuessBuilder.swift
+++ DefinitelyNotConnectionsTests/Builders/GuessBuilder.swift
@@ -7,28 +7,28 @@
 
 import Foundation
 
-struct GuessBuilder {
+class GuessBuilder {
     var clueBoxes: [ClueBox] = []
     var correctCategoryID: Int? = nil
     var oneAway: Bool = false
     var id: Int = 0
     
-    mutating func withClueBoxes(clueBoxes: [ClueBox]) -> GuessBuilder {
+    func withClueBoxes(clueBoxes: [ClueBox]) -> GuessBuilder {
         self.clueBoxes = clueBoxes
         return self
     }
     
-    mutating func withCorrectCategoryID(correctCategoryID: Int?) -> GuessBuilder {
+    func withCorrectCategoryID(correctCategoryID: Int?) -> GuessBuilder {
         self.correctCategoryID = correctCategoryID
         return self
     }
     
-    mutating func withOneAway(oneAway: Bool) -> GuessBuilder {
+    func withOneAway(oneAway: Bool) -> GuessBuilder {
         self.oneAway = oneAway
         return self
     }
     
-    mutating func withID(id: Int) -> GuessBuilder {
+    func withID(id: Int) -> GuessBuilder {
         self.id = id
         return self
     }
