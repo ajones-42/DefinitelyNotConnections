26ec1d8028487620253b232ccbf9a53fe54f3f06
ajones-42 <alexander.r.jones11@gmail.com>
2025-12-28 18:15:31 +0100

    Make CategoryBuilder a class and update boxTexts -> clueBoxTexts

--- DefinitelyNotConnectionsTests/Builders/CategoryBuilder.swift
+++ DefinitelyNotConnectionsTests/Builders/CategoryBuilder.swift
@@ -8,41 +8,41 @@
 import Foundation
 import SwiftUI
 
-struct CategoryBuilder {
+class CategoryBuilder {
     var name: String = "A Category"
-    var boxTexts: [String] = ["A clue!", "Another clue!", "Yet another clue!", "Guess what, a clue!"]
+    var clueBoxTexts: [String] = ["A clue!", "Another clue!", "Yet another clue!", "Guess what, a clue!"]
     var clueBoxes: [ClueBox] = []
     var colour: Color = .red
     var id: Int = 0
 
-    mutating func withName(name: String) -> CategoryBuilder {
+    func withName(name: String) -> CategoryBuilder {
         self.name = name
         return self
     }
     
-    mutating func withBoxTexts(boxTexts: [String]) -> CategoryBuilder {
-        self.boxTexts = boxTexts
+    func withBoxTexts(boxTexts: [String]) -> CategoryBuilder {
+        self.clueBoxTexts = boxTexts
         return self
     }
     
-    mutating func withClueBoxes(clueBoxes: [ClueBox]) -> CategoryBuilder {
+    func withClueBoxes(clueBoxes: [ClueBox]) -> CategoryBuilder {
         self.clueBoxes = clueBoxes
         return self
     }
     
-    mutating func withColour(colour: Color) -> CategoryBuilder {
+    func withColour(colour: Color) -> CategoryBuilder {
         self.colour = colour
         return self
     }
     
-    mutating func withID(id: Int) -> CategoryBuilder {
+    func withID(id: Int) -> CategoryBuilder {
         self.id = id
         return self
     }
     
     func build() -> Category {
         if self.clueBoxes.isEmpty {
-            return Category(name: self.name, boxTexts: self.boxTexts, colour: self.colour, id: self.id)
+            return Category(name: self.name, clueBoxTexts: self.clueBoxTexts, colour: self.colour, id: self.id)
         } else {
             return Category(name: self.name, clueBoxes: self.clueBoxes, colour: self.colour, id: self.id)
         }
