d78ede185ada9405908fee306c41892f52a75a69
ajones-42 <alexander.r.jones11@gmail.com>
2026-01-15 14:44:28 +0100

    Add CategoryInfo struct

---
+++ DefinitelyNotConnections/GamePlay/Categories/Category/CategoryInfo.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryInfo.swift
+//  DefinitelyNotConnections
+//
+//  Created by Alex Jones on 15.01.26.
+//
+
+import Foundation
+import SwiftUI
+
+struct CategoryInfo {
+    let name: String
+    let colour: Color
+    let clues: [String]
+}
