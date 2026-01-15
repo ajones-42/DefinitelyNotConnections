3ecbf2b023eeab4391b3b82e549e121a5b638e0d
ajones-42 <alexander.r.jones11@gmail.com>
2026-01-15 18:34:34 +0100

    Generate UUIDs in CategoryInfo on init

--- DefinitelyNotConnections/GamePlay/Categories/Category/CategoryInfo.swift
+++ DefinitelyNotConnections/GamePlay/Categories/Category/CategoryInfo.swift
@@ -12,4 +12,19 @@ struct CategoryInfo {
     let name: String
     let colour: Color
     let clues: [String]
+    let clueIDs: [UUID]
+    
+    init(name: String, colour: Color, clues: [String], clueIDs: [UUID]) {
+        self.name = name
+        self.colour = colour
+        self.clues = clues
+        self.clueIDs = clueIDs
+    }
+    
+    init(name: String, colour: Color, clues: [String]) {
+        self.name = name
+        self.colour = colour
+        self.clues = clues
+        self.clueIDs = self.clues.map( {_ in UUID()} )
+    }
 }
