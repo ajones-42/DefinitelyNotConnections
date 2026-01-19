84927d2aec6a39846557396f6356c7cee6beab3b
ajones-42 <alexander.r.jones11@gmail.com>
2026-01-19 19:58:02 +0100

    Add isCompleted to ClueBox, and add corresponding set(un)Completed methods

--- DefinitelyNotConnections/GamePlay/Categories/RemainingClueBoxes/ClueBox/ClueBox.swift
+++ DefinitelyNotConnections/GamePlay/Categories/RemainingClueBoxes/ClueBox/ClueBox.swift
@@ -9,19 +9,21 @@ import Foundation
 
 struct ClueBox: Identifiable, Equatable {
     let clue: String
+    let isCompleted: Bool
     let isSelected: Bool
     let shake: Bool
     let id: UUID
     
-    init(clue: String, isSelected: Bool, shake: Bool, id: UUID) {
+    init(clueInfo: ClueInfo) {
+        self.init(clue: clueInfo.clue, isCompleted: false, isSelected: false, shake: false, id:clueInfo.id)
+    }
+    
+    init(clue: String, isCompleted: Bool, isSelected: Bool, shake: Bool, id: UUID) {
         self.clue = clue
+        self.isCompleted = isCompleted
         self.isSelected = isSelected
         self.shake = shake
         self.id = id
-    }
-    
-    init(clueInfo: ClueInfo) {
-        self.init(clue: clueInfo.clue, isSelected: false, shake: false, id:clueInfo.id)
     }
     
     //static func ==(lhs: ClueBox, rhs: ClueBox) -> Bool {
@@ -29,18 +31,26 @@ struct ClueBox: Identifiable, Equatable {
     //}
     
     func click() -> ClueBox {
-        return ClueBox(clue: self.clue, isSelected: !self.isSelected, shake: self.shake, id: self.id)
+        return ClueBox(clue: self.clue, isCompleted: self.isCompleted, isSelected: !self.isSelected, shake: self.shake, id: self.id)
     }
     
     func deselect() -> ClueBox {
-        return ClueBox(clue: self.clue, isSelected: false, shake: self.shake, id: self.id)
+        return ClueBox(clue: self.clue, isCompleted: self.isCompleted, isSelected: false, shake: self.shake, id: self.id)
     }
     
     func startShake() -> ClueBox {
-        return ClueBox(clue: self.clue, isSelected: self.isSelected, shake: true, id: self.id)
+        return ClueBox(clue: self.clue, isCompleted: self.isCompleted, isSelected: self.isSelected, shake: true, id: self.id)
     }
     
     func stopShake() -> ClueBox {
-        return ClueBox(clue: self.clue, isSelected: self.isSelected, shake: false, id: self.id)
+        return ClueBox(clue: self.clue, isCompleted: self.isCompleted, isSelected: self.isSelected, shake: false, id: self.id)
+    }
+    
+    func setCompleted() -> ClueBox {
+        return ClueBox(clue: self.clue, isCompleted: true, isSelected: self.isSelected, shake: self.shake, id: self.id)
+    }
+    
+    func setUncompleted() -> ClueBox {
+        return ClueBox(clue: self.clue, isCompleted: false, isSelected: self.isSelected, shake: self.shake, id: self.id)
     }
 }
