e9595fef902946bbb944cc96e5bc15e9d53ba635
ajones-42 <alexander.r.jones11@gmail.com>
2026-01-26 18:44:55 +0100

    Create MainGameBuilder

---
+++ DefinitelyNotConnections/GamePlay/MainGameBuilder.swift
@@ -0,0 +1,70 @@
+//
+//  MainGameBuilder.swift
+//  DefinitelyNotConnections
+//
+//  Created by Alex Jones on 26.01.26.
+//
+
+import Foundation
+
+class MainGameBuilder {
+    var gameProperties: GameProperties
+    var gamePhase: GamePhase
+    var categories: Categories
+    var allGuesses: AllGuesses
+    var popup: Popup?
+    var mistakes: Mistakes
+    
+    init(setupInfo: SetupInfo) {
+        self.gameProperties = GameProperties(setupInfo: setupInfo)
+        self.gamePhase = .setup
+        self.categories = Categories(setupInfo: setupInfo, gameProperties: self.gameProperties)
+        self.allGuesses = AllGuesses()
+        self.popup = nil
+        self.mistakes = Mistakes(gameProperties: self.gameProperties)
+    }
+    
+    init() {
+        let setupInfo: SetupInfo = createDefaultTestSetupInfo()
+        self.gameProperties = GameProperties(setupInfo: setupInfo)
+        self.gamePhase = .setup
+        self.categories = Categories(setupInfo: setupInfo, gameProperties: self.gameProperties)
+        self.allGuesses = AllGuesses()
+        self.popup = nil
+        self.mistakes = Mistakes(gameProperties: self.gameProperties)
+    }
+    
+    func withGameProperties(gameProperties: GameProperties) -> MainGameBuilder {
+        self.gameProperties = gameProperties
+        return self
+    }
+    
+    func withGamePhase(gamePhase: GamePhase) -> MainGameBuilder {
+        self.gamePhase = gamePhase
+        return self
+    }
+    
+    func withCategories(categories: Categories) -> MainGameBuilder {
+        self.categories = categories
+        return self
+    }
+    
+    func withAllGuesses(allGuesses: AllGuesses) -> MainGameBuilder {
+        self.allGuesses = allGuesses
+        return self
+    }
+    
+    func withPopup(popup: Popup) -> MainGameBuilder {
+        self.popup = popup
+        return self
+    }
+    
+    func withMistakes(mistakes: Mistakes) -> MainGameBuilder {
+        self.mistakes =  mistakes
+        return self
+    }
+    
+    func build() -> MainGame {
+        return MainGame(gameProperties: self.gameProperties, gamePhase: self.gamePhase, categories: self.categories, allGuesses: self.allGuesses, popup: self.popup, mistakes: self.mistakes)
+    }
+}
