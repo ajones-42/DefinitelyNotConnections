4a121ace0ceb747f064b3950742f7a8aa467003d
ajones-42 <alexander.r.jones11@gmail.com>
2026-02-22 13:40:41 +0100

    Fix AllGuessesTests and test adding same guess throws

--- DefinitelyNotConnectionsTests/AllGuessesTests.swift
+++ DefinitelyNotConnectionsTests/AllGuessesTests.swift
@@ -6,6 +6,7 @@
 //
 
 import Testing
+import Foundation
 @testable import DefinitelyNotConnections
 
 @Suite struct AllGuessesTests {
@@ -33,11 +34,29 @@ import Testing
         #expect(allGuesses.guesses.count == 1 && allGuesses.guesses.last == newGuess)
     }
 
-    @Test func testAddGuess() {
-        let allGuesses: AllGuesses = AllGuessesBuilder().withGuesses(guesses: [GuessBuilder().build()]).build()
-        let newGuess: Guess = GuessBuilder().build()
+    @Test func testAddNewGuess() {
+        let clueInfo1: ClueInfo = ClueInfo(clue: "Clue1", connectionsCategoryID: UUID())
+        let clueInfo2: ClueInfo = ClueInfo(clue: "Clue2", connectionsCategoryID: UUID())
+        let clueInfo3: ClueInfo = ClueInfo(clue: "Clue3", connectionsCategoryID: UUID())
+        let clueInfo4: ClueInfo = ClueInfo(clue: "Clue4", connectionsCategoryID: UUID())
+        let allGuesses: AllGuesses = AllGuessesBuilder().withGuesses(guesses: [GuessBuilder().withClueInfos(clueInfos: [clueInfo1, clueInfo2]).build()]).build()
+        let newGuess: Guess = GuessBuilder().withClueInfos(clueInfos: [clueInfo3, clueInfo4]).build()
         try! allGuesses.addGuess(guess: newGuess)
 
         #expect(allGuesses.guesses.count == 2 && allGuesses.guesses.last == newGuess)
+    }
+    
+    @Test func testAddSameGuess() {
+        let clueInfo1: ClueInfo = ClueInfo(clue: "Clue1", connectionsCategoryID: UUID())
+        let clueInfo2: ClueInfo = ClueInfo(clue: "Clue2", connectionsCategoryID: UUID())
+        let clueInfos: [ClueInfo] = [clueInfo1, clueInfo2]
+        let oldGuess: Guess = GuessBuilder().withClueInfos(clueInfos: clueInfos).build()
+        let allGuesses: AllGuesses = AllGuessesBuilder().withGuesses(guesses: [oldGuess]).build()
+        let newGuess: Guess = GuessBuilder().withClueInfos(clueInfos: clueInfos).build()
+        #expect(throws: ValidationError.alreadyGuessed) {
+            try allGuesses.addGuess(guess: newGuess)
+        }
+
+        #expect(allGuesses.guesses.count == 1 && allGuesses.guesses.last == oldGuess)
     }
 }
